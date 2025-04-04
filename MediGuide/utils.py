481c96059558399f519098fdb7d9be722fbdcbf3
nick <nickchen1998@gmail.com>
2025-04-05 02:55:26 +0800

    重新整理：重新整理MediGuide中的文件结构和依赖关系
    - 删除 `MediGuide/mongo.py` 文件
    - 添加 `tomllib` 和 `pathlib` 导入到 `MediGuide/utils.py`
    - 更新 `MediGuide/utils.py` 中的 `get_mongo_database` 函数以处理缺失的环境变量
    
    Signed-off-by: nick <nickchen1998@gmail.com>

--- MediGuide/utils.py
+++ MediGuide/utils.py
@@ -1,8 +1,21 @@
 import os
 import time
+import tomllib
+import pathlib
 import tempfile
+import contextlib
 import streamlit as st
+
+from uuid import uuid4
 from openai import OpenAI
+
+from pymongo.database import Database
+from pymongo.collection import Collection
+from pymongo.mongo_client import MongoClient
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents import Document
+from langchain_mongodb.vectorstores import MongoDBAtlasVectorSearch
 
 
 def write_history():
@@ -47,3 +60,38 @@ def get_record_text_by_whisper(audio_bytes: bytes):
         )
 
     return translation.text
+
+
+@contextlib.contextmanager
+def get_mongo_database() -> Database:
+    if os.getenv("MONGODB_URI") is None:
+        secret_file = pathlib.Path(__file__).parent.parent / "secrets.toml"
+        with open(secret_file, "rb") as f:
+            config = tomllib.load(f)
+        os.environ["MONGODB_URI"] = config["MONGODB_URI"]
+
+    client = MongoClient(host=os.getenv("MONGODB_URI"))
+    try:
+        yield Database(client, name="IllnessQA")
+    finally:
+        client.close()
+
+
+def insert_illness_datas(datas: list):
+    with get_mongo_database() as database:
+        vector_store = MongoDBAtlasVectorSearch(
+            collection=Collection(database, name="illness"),
+            embedding=OpenAIEmbeddings(model="text-embedding-3-small", api_key=st.secrets["OPENAI_API_KEY"]),
+            index_name="illness_refactor_question",
+            relevance_score_fn="cosine",
+        )
+
+        documents = []
+        for data in datas:
+            documents.append(Document(
+                page_content=data.pop("refactor_question"),
+                metadata=data
+            ))
+
+        uuids = [str(uuid4()) for _ in range(len(documents))]
+        vector_store.add_documents(documents, uuids)
