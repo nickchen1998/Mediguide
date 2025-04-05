e86b729b7b61ffdf8c4d1b9a83541b96fa2860f7
nick <nickchen1998@gmail.com>
2025-04-05 14:27:47 +0800

    特性：優化網頁爬取流程和瀏覽器設定
    - 將瀏覽器驅動程序從Safari改為Chrome
    - 添加用戶代理隨機化
    - 通過分頁改進數據提取流程
    - 更新相依性：將 `fake-useragent` 添加到 `pyproject.toml`
    
    Signed-off-by: nick <nickchen1998@gmail.com>

--- MediGuide/crawlers.py
+++ MediGuide/crawlers.py
@@ -1,67 +1,80 @@
+import random
 import re
 import time
 import json
 import utils
 import pathlib
 from datetime import datetime
-from selenium.webdriver import Safari
+from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
 
-dataset_path = pathlib.Path(__file__).parent / "datasets.json"
-with open(dataset_path, "r", encoding="utf-8") as f:
-    datasets = json.load(f)
 
-browser = Safari()
+def get_paragraph(chrome: Chrome):
+    for paragraph in chrome.find_elements(By.CSS_SELECTOR, "ul.QAunit"):
+        subject = paragraph.find_element(By.CSS_SELECTOR, "li.subject").text
 
-for dataset in datasets:
-    browser.get(dataset["start_url"])
+        asker_info = paragraph.find_element(By.CSS_SELECTOR, "li.asker").text
+        match = re.search(r'／([男女])／.*?,(\d{4}/\d{2}/\d{2})', asker_info)
+        gender = match.group(1)
+        question_time = datetime.strptime(match.group(2), '%Y/%m/%d')
 
-    symptom_select_menu = browser.find_element(By.CSS_SELECTOR, "select[name='q_type']")
-    symptom_list = [tmp.get_attribute("value") for tmp in symptom_select_menu.find_elements(
-        By.TAG_NAME, "option") if tmp.get_attribute("value")]
+        question = paragraph.find_element(By.CSS_SELECTOR, "li.ask").text
 
-    for symptom in symptom_list:
-        browser.get(
-            f"https://sp1.hso.mohw.gov.tw/doctor/Often_question/type_detail.php?"
-            f"q_type={symptom}&UrlClass={dataset['department']}"
+        answer = paragraph.find_element(By.CSS_SELECTOR, "li.ans").text
+        answer_time = datetime.strptime(match.group(2), '%Y/%m/%d')
+
+        data = dict(
+            subject_id=int(subject.split(" ")[0].replace("#", "")),
+            subject="".join(subject.split(" ")[1:]),
+            symptom=symptom,
+            question=question,
+            gender=gender,
+            question_time=question_time,
+            answer=answer,
+            department=dataset["department"],
+            answer_time=answer_time,
         )
-        datas = []
-        for paragraph in browser.find_elements(By.CSS_SELECTOR, "ul.QAunit"):
-            time.sleep(10)
+        yield data
 
-            subject = paragraph.find_element(By.CSS_SELECTOR, "li.subject").text
 
-            asker_info = paragraph.find_element(By.CSS_SELECTOR, "li.asker").text
-            match = re.search(r'／([男女])／.*?,(\d{4}/\d{2}/\d{2})', asker_info)
-            gender = match.group(1)
-            question_time = datetime.strptime(match.group(2), '%Y/%m/%d')
+if __name__ == '__main__':
 
-            question = paragraph.find_element(By.CSS_SELECTOR, "li.ask").text
+    dataset_path = pathlib.Path(__file__).parent / "datasets.json"
+    with open(dataset_path, "r", encoding="utf-8") as f:
+        datasets = json.load(f)
 
-            answer = paragraph.find_element(By.CSS_SELECTOR, "li.ans").text
-            answer_time = datetime.strptime(match.group(2), '%Y/%m/%d')
+    options = Options()
+    # options.add_argument("--headless")
+    options.add_argument(f'user-agent={UserAgent().random}')
+    browser = Chrome(options=options)
+    browser.maximize_window()
 
-            data = dict(
-                subject_id=int(subject.split(" ")[0].replace("#", "")),
-                subject="".join(subject.split(" ")[1:]),
-                symptom=symptom,
-                question=question,
-                gender=gender,
-                question_time=question_time,
-                answer=answer,
-                department=dataset["department"],
-                answer_time=answer_time,
-            )
+    for dataset in datasets:
+        browser.get(dataset["start_url"])
 
-            datas.append(data)
+        symptom_select_menu = browser.find_element(By.CSS_SELECTOR, "select[name='q_type']")
+        symptom_list = [tmp.get_attribute("value") for tmp in symptom_select_menu.find_elements(
+            By.TAG_NAME, "option") if tmp.get_attribute("value")]
 
-            try:
-                next_page_element = browser.find_element(By.LINK_TEXT, "下一頁")
-                next_page_element.click()
-            except NoSuchElementException:
-                break
-        if datas:
-            utils.insert_symptom_subject_datas(datas)
+        for symptom in symptom_list:
+            time.sleep(random.randint(4, 8))
+            url = (f"https://sp1.hso.mohw.gov.tw/doctor/Often_question/type_detail.php?"
+                   f"q_type={symptom}&UrlClass={dataset['department']}")
+            browser.get(url)
+            datas = []
+            page = 1
 
-browser.quit()
+            while browser.find_elements(By.CSS_SELECTOR, "ul.QAunit"):
+                datas.extend(list(get_paragraph(browser)))
+
+                page += 1
+                tmp_url = url + f"&PageNo={page}"
+                time.sleep(random.randint(4, 8))
+                browser.get(tmp_url)
+
+            if datas:
+                utils.insert_symptom_subject_datas(datas)
+
+    browser.quit()
